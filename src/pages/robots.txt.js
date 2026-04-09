5216fdea3c1756bb851335680a25f68169682715
mohammed dhinojwala <kiritokazugaya2005@gmail.com>
2026-04-09 15:31:39 +0530

    sitemap & robot added

--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -1,13 +1,9 @@
-export default function Robots() {
-  return null;
-}
-
-export async function getServerSideProps({ res }) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write(`User-agent: *
-Allow: /
-Sitemap: https://www.alllstars.com/sitemap.xml`);
-  res.end();
-
-  return { props: {} };
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.alllstars.com/sitemap.xml',
+  };
 }
